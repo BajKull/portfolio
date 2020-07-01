84d6f343f17982d811544579f3a17390d27ccfad
Dawid Banachowski <bajkull2@gmail.com>
2020-07-01 14:53:27 +0200

    upload

---
+++ src/main/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div className="mainPage">
+      <h1> Mam to coś </h1>
+      <h4> Witam, nazywam się Dawid, a to moje portfolio! </h4>
+    </div>
+  );
+}
---
+++ src/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
