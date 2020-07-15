7e3ae90b6632524ede4533c2038dcc271ec9a5f8
Dawid Banachowski <bajkull2@gmail.com>
2020-07-15 20:41:03 +0200

    mobile vh fix on main page

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import NavButton from "../navbar/NavButton";
 
 export default function Main() {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    window.addEventListener("resize", () => {
+      vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
+  }, []);
   return (
     <div className="mainPage">
       <div className="mainBg"></div>
